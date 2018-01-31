6804a1b2f1179988fffce95812435c39a2b6badb
mykurisu <452454162@qq.com>
2018-01-31 16:52:15 +0800

    组件注释

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// Item组件--所有格子的操作都可以在此进行，如果这些操作都能与"activedId"关联就更好了
 class RowItem extends Component {
   render() {
     const { content, activedId } = this.props;
@@ -16,16 +17,24 @@ class App extends Component {
   constructor() {
     super()
     this.state = {
+      // 九宫格内容list
       list: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+      // 被选中的格子的ID
       activedId: '',
+      // 中奖ID
       prizeId: null,
+      // 获得prizeId之后计算出的动画次数
       times: 0,
+      // 当前动画次数
       actTimes: 0,
-      isRolling: false,
+      // 是否正在抽奖
+      isRolling: false
     }
   }
-  handleBegin () {
+  handleBegin() {
+    // this.state.isRolling为false的时候才能开始抽，不然会重复抽取，造成无法预知的后果
     if (!this.state.isRolling) {
+      // 点击抽奖之后，我个人做法是将于九宫格有关的状态都还原默认
       this.setState({
         activedId: '',
         prizeId: null,
@@ -33,25 +42,30 @@ class App extends Component {
         actTimes: 0,
         isRolling: true
       }, () => {
+        // 状态还原之后才能开始真正的抽奖
         this.handlePlay()
-      }) 
+      })
     }
   }
   handlePlay() {
+    // 随机获取一个中奖ID
     let prize = Math.floor(Math.random() * 12)
     console.log(prize)
     this.setState({
       prizeId: prize,
       activedId: 0
     })
+    // 随机算出一个动画执行的最小次数，这里可以随机变更数值，按自己的需求来
     let times = this.state.list.length * Math.floor(Math.random() * 5 + 4)
     this.setState({
       times: times
     })
+    // 抽奖正式开始↓↓
     this.begin = setInterval(() => {
       let num;
 
       if (this.state.activedId === this.state.prizeId && this.state.actTimes > this.state.times) {
+        // 符合上述所有条件时才是中奖的时候，两个ID相同并且动画执行的次数大于(或等于也行)设定的最小次数
         clearInterval(this.begin)
         this.setState({
           isRolling: false
@@ -59,6 +73,7 @@ class App extends Component {
         return
       }
 
+      // 以下是动画执行时对id的判断
       if (this.state.activedId === '') {
         num = 0
         this.setState({
